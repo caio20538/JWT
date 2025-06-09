16ec94db9ecc23069541bc96a796251308417f2f
caio20538 <caio20538@gmail.com>
2025-06-08 21:51:20 -0300

    fix
    Signed-off-by: caio20538 <caio20538@gmail.com>

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_roles (name)
+SELECT 'ADMIN'
+WHERE NOT EXISTS (SELECT 1 FROM tb_roles WHERE name = 'ADMIN');
+
+INSERT INTO tb_roles (name)
+SELECT 'ADMIN'
+WHERE NOT EXISTS (SELECT 1 FROM tb_roles WHERE name = 'ADMIN');
